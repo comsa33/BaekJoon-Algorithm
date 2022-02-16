56eeb76654f66a18d4c14f6f47ea33742b932efd
Ruo Lee <ruo@Ruos-MacBook-Pro.local>
2022-02-16 17:45:11 +0900

    9372.py

---
+++ 9372.py
@@ -0,0 +1,8 @@
+import sys
+
+T = int(sys.stdin.readline())
+for _ in range(T):
+    N, F = map(int, sys.stdin.readline().rstrip().split())
+    for _ in range(F):
+        c1, c2 = sys.stdin.readline().rstrip().split()
+    print(N-1)
